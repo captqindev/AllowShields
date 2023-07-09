c9b8d8b9757c6b30e62b77bb14c8b8e5eebbca44
captqindev <135941078+captqindev@users.noreply.github.com>
2023-07-09 16:21:14 -0700

    Create Shield.php

---
+++ src/CaptqinDev/AllowShields/item/Shield.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace CaptqinDev\Shield\item;
+
+use pocketmine\block\Block;
+use pocketmine\item\Durable;
+use pocketmine\item\Item;
+
+class Shield extends Durable{
+
+	public function getMaxStackSize(): int{
+		return 1;
+	}
+
+	public function getMaxDurability(): int{
+		return 337;
+	}
+
+	/**
+	 * @param Item[] &$returnedItems
+	 */
+	public function onDestroyBlock(Block $block, array &$returnedItems): bool{
+		if(!$block->getBreakInfo()->breaksInstantly()){
+			return $this->applyDamage(2);
+		}
+		return false;
+	}
+}
